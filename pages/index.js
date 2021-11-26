84776c53b4df1fdabc573caeefc1e74719673406
Admin <wiwm07@gmail.com>
2021-11-26 20:33:34 +0800

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import {
-  getFirestore,
-  collection,
-  onSnapshot,
-  addDoc,
-  serverTimestamp,
-  query,
-  orderBy,
-  limit,
-} from "firebase/firestore";
+import Head from "next/head";
 import { initializeApp } from "firebase/app";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore, collection, onSnapshot, addDoc, serverTimestamp, query, orderBy } from "firebase/firestore";
 
-const app = initializeApp({
+initializeApp({
   apiKey: "AIzaSyCOc_h3YcJE7EC2xGIB0WQKmKc7FyjaHEc",
   authDomain: "techquid-web.firebaseapp.com",
   projectId: "techquid-web",
@@ -22,60 +14,48 @@ const app = initializeApp({
   appId: "1:596423863198:web:be36a8ca54b0145db3448f",
 });
 
-const auth = getAuth();
 const db = getFirestore();
 
 function Chat() {
   const [data, setData] = useState([]);
   const [message, setMessage] = useState("");
 
-  useEffect(async () => {
-    await onSnapshot(
-      query(collection(db, "Chat"), orderBy("createdAt")),
-      (snapshot) => {
-        const data = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
+  const dummy = useRef();
+  
+  useEffect(() => onSnapshot(query(collection(db, "Chat"), orderBy("createdAt")), (snapshot) => setData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))), []);
 
-        setData(data);
-      }
-    );
-  }, []);
-
-  async function sendMessage(e) {
+  async function send(e) {
     e.preventDefault();
 
-    if (message == "") return;
-
-    await addDoc(collection(db, "Chat"), {
-      name: auth.currentUser.displayName,
-      message: message,
-      createdAt: serverTimestamp(),
-    });
-
-    setMessage("");
+    if (message === "") { return }
+    else if (message === "/signout") { signOut(getAuth()) } 
+    else {
+      await addDoc(collection(db, "Chat"), { name: getAuth().currentUser.providerData[0].displayName, message: message, createdAt: serverTimestamp() });
+      setMessage("");
+      dummy.current.scrollIntoView({ behavior: "smooth" });
+    }
   }
 
   return (
-    <main className="Chat">
+    <main className="chat">
+      <Head>
+        <title>Chat</title>
+      </Head>
+
       <ul>
-        {data.map((msg) => {
+        {data.map((info) => {
           return (
-            <li id="message" key={msg.id}>
-              <h1>{msg.name}</h1>
-              <p>{msg.message}</p>
+            <li key={info.id}>
+              <h1>{info.name}</h1>
+              <p>{info.message}</p>
             </li>
           );
         })}
+        <div ref={dummy}></div>
       </ul>
 
-      <form onSubmit={sendMessage}>
-        <input
-          value={message}
-          onChange={(msg) => setMessage(msg.target.value)}
-          placeholder="Message"
-        ></input>
+      <form onSubmit={send}>
+        <input value={message} onChange={(value) => setMessage(value.target.value)} placeholder="Message"></input>
         <button type="submit">Send</button>
       </form>
     </main>
@@ -85,16 +65,10 @@ function Chat() {
 function LogIn() {
   return (
     <main className="login">
-      <button
-        id="continue-with-google"
-        onClick={() =>
-          signInWithPopup(auth, new GoogleAuthProvider()).then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-          })
-        }
-      >
+      <Head>
+        <title>Log In</title>
+      </Head>
+      <button id="continue-with-google" onClick={() => signInWithPopup(getAuth(), new GoogleAuthProvider())}>
         Continue with Google
       </button>
     </main>
@@ -102,6 +76,6 @@ function LogIn() {
 }
 
 export default function App() {
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(getAuth());
   return user ? <Chat /> : <LogIn />;
 }
