176c4f87c3f0d3335f69997b077d28cfd899841b
Admin <wiwm07@gmail.com>
2021-11-28 17:53:49 +0800

    firebase > supabase

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,58 @@
-import Head from "next/head";
-import { initializeApp } from "firebase/app";
-import { useEffect, useState } from "react";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, collection, onSnapshot, addDoc, serverTimestamp, query, orderBy } from "firebase/firestore";
+import { createClient } from "@supabase/supabase-js"
+import { useState, useEffect } from "react"
 
-initializeApp({
-  apiKey: "AIzaSyCOc_h3YcJE7EC2xGIB0WQKmKc7FyjaHEc",
-  authDomain: "techquid-web.firebaseapp.com",
-  projectId: "techquid-web",
-  storageBucket: "techquid-web.appspot.com",
-  messagingSenderId: "596423863198",
-  appId: "1:596423863198:web:be36a8ca54b0145db3448f"
-});
+const supabase = createClient("https://ukkudmxrtbwpjltpzlgj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODA4MDYwNSwiZXhwIjoxOTUzNjU2NjA1fQ.m8w2kOL2jjD4WoyZCwpy4Aper-8Gvw05Tomx2Yop7ik")
+const user = supabase.auth.user()
 
-const db = getFirestore();
+export default function App() {
+  return user ? <Chat/> : <SignIn/>
+}
+
+function SignIn() {
+  const signInWithDiscord = async () => await supabase.auth.signIn({provider:"discord"})
+  const signInWithGitHub = async () => await supabase.auth.signIn({provider:"github"})
+
+  supabase.from("messages").on("*", data => console.log(data)).subscribe()
+
+  return (
+    <div className="signin">
+      <button onClick={signInWithDiscord}>Continue with Discord</button>
+      <button onClick={signInWithGitHub}>Continue with GitHub</button>
+    </div>
+  )
+}
+
 
 function Chat() {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("")
+  const [messages, setMessages] = useState()
   
-  useEffect(() => onSnapshot(query(collection(db, "Chat"), orderBy("createdAt")), (data) => setMessages(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))), []);
-
-  async function send(e) {
-    e.preventDefault();
-
-    switch (input) {
-      case "/signout":
-        signOut(getAuth());
-        break;
-      case "":
-        return;
-      default:
-        await addDoc(collection(db, "Chat"), { name: getAuth().currentUser.providerData[0].displayName, message: input, createdAt: serverTimestamp() });
-        setInput("");
-    }
+  async function sendMessage() {
+    await supabase.from("messages").insert([{name: user.user_metadata.name, message: input}])
   }
 
+  useEffect(async () => {
+    const {data, error} = await supabase.from('messages').select()
+    setMessages(data)
+  }, [])
+  
   return (
     <div className="chat">
       <ul>
-        {messages.map((info) => {
+        {messages.map((msg) => {
           return (
-            <li key={info.id}>
-              <h1>{info.name}</h1>
-              <p>{info.message}</p>
+            <li key={msg.created_at}>
+              <h1>{msg.name}</h1>
+              <p>{msg.message}</p>
             </li>
-          );
+          )
         })}
       </ul>
 
-      <form onSubmit={send}>
-        <input value={input} onChange={(input) => setInput(input.target.value)} placeholder="Message"></input>
-        <button type="submit">Send</button>
+      <form>
+        <input value={input} onChange={(input) => setInput(input.target.value)}/>
+        <button onClick={sendMessage}>Send</button>
       </form>
     </div>
-  );
-}
-
-function LogIn() {
-  return (
-    <div className="login">
-      <button id="continue-with-google" onClick={() => signInWithPopup(getAuth(), new GoogleAuthProvider())}>
-        Continue with Google
-      </button>
-    </div>
-  );
-}
-
-export default function App() {
-  const [user, setUser] = useState([]);
-  onAuthStateChanged(getAuth(), (user) => user ? setUser(user) : setUser(null))
-  return (
-    <>
-      <Head>
-        <title>TQ Chat</title>
-      </Head>
-      {user ? <Chat /> : <LogIn />}
-    </>
-  ) 
+  )
 }
